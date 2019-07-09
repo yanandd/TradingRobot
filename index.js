87e58939c440ed6e935a360eb35a6eb42c0274f9
ouyuanmi1981 <ouyuanmi@gmail.com>
2019-07-10 06:18:10 +0900

    init

---
+++ index.js
@@ -0,0 +1,41 @@
+// const WebSocket = require("rpc-websockets").Client;
+
+// const channelName = "lightning_executions_FX_BTC_JPY";
+
+// // note: rpc-websockets supports auto-reconection.
+// const ws = new WebSocket("wss://ws.lightstream.bitflyer.com/json-rpc");
+
+// ws.on("open", () => {
+//     ws.call("subscribe", {
+//         channel: channelName
+//     });
+// });
+
+// ws.on("channelMessage", notify => {
+//     console.log(notify.channel, notify.message);
+// });
+
+var express = require('express');
+var app = express();
+
+app.get('/', function (res, rep) {
+    const WebSocket = require("rpc-websockets").Client;
+
+    const channelName = "lightning_executions_FX_BTC_JPY";
+
+    // note: rpc-websockets supports auto-reconection.
+    const ws = new WebSocket("wss://ws.lightstream.bitflyer.com/json-rpc");
+
+    ws.on("open", () => {
+        ws.call("subscribe", {
+            channel: channelName
+        });
+    });
+
+    ws.on("channelMessage", notify => {
+        console.log(notify.channel, notify.message);
+    });
+    rep.send('Hello, word!');
+});
+
+app.listen(3000);
